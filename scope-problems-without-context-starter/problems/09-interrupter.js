cac0bcf0a928edaff70402484470dbfe02f2b50e
Steve <stevemuiyuro@gmail.com>
2022-12-12 15:16:50 +0300

    Add scope_problems_without_context

---
+++ scope-problems-without-context-starter/problems/09-interrupter.js
@@ -0,0 +1,39 @@
+/***********************************************************************
+Write a function named: interrupter(interruptingWord). The interrupter function will
+accept a word and return a function. When the function returned by interrupter
+is invoked with a string the string will be returned with "interruptions".
+
+Look below to see how this function is invoked:
+let rudePerson = interrupter("what"); // => returns a function
+console.log(rudePerson("how are you")); // prints "how what are what you"
+console.log(rudePerson("I like pie")); // prints "I what like what pie"
+
+
+Invoking the interrupter function again:
+let rudePerson2 = interrupter("yo"); // => returns a function
+console.log(rudePerson2("I love dogs")); // prints "I yo love yo dogs"
+
+
+***********************************************************************/
+
+// your code here!
+let interrupter = (word) => {
+  return function (str) {
+    let newWord = "";
+    let array = str.split(" ");
+    for (let i = 0; i < array.length; i += 2) {
+      let newArray = array.splice(i, 0, word);
+    }
+    newWord = newArray.join("");
+    return newWord;
+  };
+};
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+
+try {
+  module.exports = interrupter;
+} catch (e) {
+  // catch the ref err
+  module.exports = null;
+}
